77b1617d493dcb3dabe5bec0aad20046e7f2c72f
sammy2342 <sammyfares303@gmail.com>
2022-10-21 16:56:18 -0400

    complete base html page

--- main_app/views.py
+++ main_app/views.py
@@ -5,4 +5,7 @@ from django.http import HttpResponse
 def home(request): 
     return HttpResponse('<h1>Hello Welcome</h1>')
 
+def about(request): 
+    return render(request, 'about.html')
+
 # Create your views here.
